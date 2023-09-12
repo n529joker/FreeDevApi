02546ad32f66ab97ee641958cb35330d1217634a
Rawlings Ngenge <69519770+n529joker@users.noreply.github.com>
2023-09-12 08:07:08 +0100

    //sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,26 @@
-const nodemailer = require('nodemailer')
-const emailTemplate = require('../views/verifyEmail')
+// const nodemailer = require('nodemailer')
+// const emailTemplate = require('../views/verifyEmail')
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
-    }
-})
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.PASS
+//     }
+// })
 
 
 
-const sendEmail = async (options) => {
-    const {email, id} = options
-    const html = emailTemplate(id)
-    const mailOptions = {
-        from: process.env.EMAIL,
-        to: email,
-        subject: 'Email Verification',
-        html: html
-    }
+// const sendEmail = async (options) => {
+//     const {email, id} = options
+//     const html = emailTemplate(id)
+//     const mailOptions = {
+//         from: process.env.EMAIL,
+//         to: email,
+//         subject: 'Email Verification',
+//         html: html
+//     }
 
-    await transporter.sendMail(mailOptions, )
+//     await transporter.sendMail(mailOptions, )
 
-}
+// }
